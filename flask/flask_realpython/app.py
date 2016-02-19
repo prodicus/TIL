86ccc8b056d9c95c2778b3b39f890c754de0e99e
Tasdik Rahman <tasdik95@gmail.com>
2016-02-19 19:45:11 +0530

    init nltk

--- flask/flask_realpython/app.py
+++ flask/flask_realpython/app.py
@@ -4,25 +4,35 @@
 # @Date:   2016-02-08
 # @Email:  prodicus@outlook.com  Github username: @prodicus
 # @Last Modified by:   tasdik
-# @Last Modified time: 2016-02-08
+# @Last Modified time: 2016-02-18
 # MIT License. You can find a copy of the License @
 # http://prodicus.mit-license.org
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return 'Hello world'
 
 
-@app.route('/welcome')
+@app.route('/welcome/')
 def welcome():
-    return render_template("welcome.html")
+    return render_template('welcome.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid username and password'
+        else:
+            return redirect(url_for('welcome'))
+    return render_template('login.html', error=error)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, port=3000)
